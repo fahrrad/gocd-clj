2d33cf52e57f7984a4e2df08da62f7d541655855
fahhrad <ward.coessens@gmail.com>
2019-06-15 19:41:21 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gocd-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gocd_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns gocd-clj.core-test
+  (:require [clojure.test :refer :all]
+            [gocd-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
